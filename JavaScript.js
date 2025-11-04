f294c00486fe1b1498b1d7357bf4f4e0b9465840
JuanGTZ07 <al45677@un.edu.mx>
2025-11-04 17:32:26 -0600

    hamburgueso

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,18 @@
-var formulario = document.getElementById("cantidad")
-
+var formulario = document.getElementById("calcularPrecio");
+var cantidad = document.getElementById("cantidad").value;
 function calcular() {
-    alert("si funciona el boton");
-    alert(formulario);
+    //var tamC = document.getElementById("chico").value;
+    var tamaño = formulario.tam.value;
+    alert(tamaño);
+    var total = 0;
+    if(tamaño == chico){
+        total = 100;
+    }
+    else if (tamaño == mediano){
+        total = 150
+    }
+    else{
+        total= 200
+    }
+    formulario.total.value = total;
 }
