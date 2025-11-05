2490f6dfc45c4cd12fb293b15d8e565f4edf2176
JuanGTZ07 <al45677@un.edu.mx>
2025-11-04 18:53:07 -0600

    ???

--- JavaScript.js
+++ JavaScript.js
@@ -1,18 +1,46 @@
 var formulario = document.getElementById("calcularPrecio");
-var cantidad = document.getElementById("cantidad").value;
+
 function calcular() {
-    //var tamC = document.getElementById("chico").value;
-    var tamaño = formulario.tam.value;
-    alert(tamaño);
     var total = 0;
+    var cantidad = document.getElementById("cantidad").value;
+    // TAMAÑO HAMBURGUESA
+    var tamaño = formulario.tam.value;
     if(tamaño == chico){
         total = 100;
     }
     else if (tamaño == mediano){
-        total = 150
+        total = 150;
     }
     else{
-        total= 200
+        total= 200;
     }
-    formulario.total.value = total;
+
+    //TIPO PAN
+    var pan = formulario.pan.value;
+    if(pan == "integral") {
+        total = total + 15;
+    }
+    else if(pan == "blanco") {
+        total = total + 0;
+    }
+    else if(pan == "papa") {
+        total = total + 20;
+    }
+    else {
+        total = total + 22;
+    }
+    
+    
+    //INGREDIENTES
+    var ing = formulario.ingredientes.checked;
+    if( ing == "QuesoExtra"){
+        total = total + 15;
+    }
+    else if( ing == "CarneExtra") {
+        total = total + 20;
+    }
+    else {
+        total = total + 15;
+    }
+    alert(total)
 }
